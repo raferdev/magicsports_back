e29aa1f6f1dd5f17b875309f8679696ae7a95b4a
Nikolas <nkls99@gmail.com>
2022-05-11 16:56:11 -0300

    pequenas mudanças e criaçao de pastas

--- database/db.js
+++ database/db.js
@@ -1,8 +1,10 @@
 import chalk from "chalk";
 import "dotenv/config";
 import { MongoClient } from "mongodb";
+
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
+
 try {
     await mongoClient.connect();
     db = mongoClient.db("magic-sports");
@@ -10,4 +12,5 @@ try {
 } catch(e) {
     console.log(chalk.red("ERRO","- ao tentar conectar a database mongo DB",e))
 }
+
 export default db;
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import authRoute from "./routes/authRoute.js";
+import authRoute from "./routes/authRouter.js";
+
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 // rotas
 app.use(authRoute);
+
 app.listen(process.env.PORT);
