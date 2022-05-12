99f6e1074a394b57a0e55eb1285f78b068b34eff
Rafael <rafinha_fern@hotmail.com>
2022-05-12 18:05:30 -0300

    pasta 'crontrollers' para 'controllers'

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import postSignUp from "../crontrollers/postSignUp.js";
+import postSignUp from "../controllers/postSignUp.js";
 import emailVerifyMiddleware from "../middlewares/emailVerifyMiddleware.js";
 import signUpJoiMiddleware from "../middlewares/signUpJoiMiddleware.js";
 
