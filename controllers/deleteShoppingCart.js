4c485193ee6bf90cc6f2da8af7a209235ff1144b
Rafael <rafinha_fern@hotmail.com>
2022-05-16 04:14:59 -0300

    deleteControll

--- controllers/deleteShoppingCart.js
+++ controllers/deleteShoppingCart.js
@@ -1,8 +1,13 @@
 import db from "../database/db.js";
+import jwt from "jsonwebtoken";
 
 async function deleteShoppingCart(req,res) {
+    const authorization = req.headers.authorization;
+    const filteredToken = authorization.replace("Bearer", "").trim();
     const id = req.query.id;
     try {
+        const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
+        const email = dados.email;
        await db.collection("shopping").update({email},{$pull:{id}})
         res.sendStatus(201);
     } catch(e) {
