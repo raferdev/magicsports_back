f3c043411cb658d048fd1ccf24ac920206ef5b1c
Rafael <rafinha_fern@hotmail.com>
2022-05-16 04:36:25 -0300

    console.log para testar funçao delet

--- controllers/deleteShoppingCart.js
+++ controllers/deleteShoppingCart.js
@@ -1,19 +1,23 @@
 import db from "../database/db.js";
 import jwt from "jsonwebtoken";
 
-async function deleteShoppingCart(req,res) {
-    const authorization = req.headers.authorization;
-    const filteredToken = authorization.replace("Bearer", "").trim();
-    const id = req.query.id;
-    console.log
-    try {
-        const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
-        const email = dados.email;
-       await db.collection("shopping").update({email},{"$pull":{id}});
-        res.sendStatus(201);
-    } catch(e) {
-        res.sendStatus(500);
-    }
-
+async function deleteShoppingCart(req, res) {
+  const authorization = req.headers.authorization;
+  const filteredToken = authorization.replace("Bearer", "").trim();
+  const id = req.query.id;
+  console.log(id);
+  console.log;
+  try {
+    const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
+    const email = dados.email;
+    console.log(email);
+    const del = await db
+      .collection("shopping")
+      .update({ email }, { $pull: { id } });
+    console.log(del);
+    res.sendStatus(201);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 }
 export default deleteShoppingCart;
