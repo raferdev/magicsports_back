53c2a44ffec7290b9dd2b2776806567ff48a255b
Rafael <rafinha_fern@hotmail.com>
2022-05-16 04:44:59 -0300

    editando o  do delete

--- controllers/deleteShoppingCart.js
+++ controllers/deleteShoppingCart.js
@@ -5,16 +5,12 @@ async function deleteShoppingCart(req, res) {
   const authorization = req.headers.authorization;
   const filteredToken = authorization.replace("Bearer", "").trim();
   const id = req.query.id;
-  console.log(id);
-  console.log;
   try {
     const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
     const email = dados.email;
-    console.log(email);
-    const del = await db
+    await db
       .collection("shopping")
-      .update({ email }, { $pull: { id } });
-    console.log(del);
+      .update({ email }, { $pull: { cart: { id } } });
     res.sendStatus(201);
   } catch (e) {
     res.sendStatus(500);
