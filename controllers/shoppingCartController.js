4d54033e2d5ce4fbb0406352b6b3071e746d7872
Rafael <rafinha_fern@hotmail.com>
2022-05-16 02:30:17 -0300

    ultimas alterações pós teste token shopping controller

--- controllers/shoppingCartController.js
+++ controllers/shoppingCartController.js
@@ -4,9 +4,10 @@ import db from "../database/db.js";
 async function shoppingCartController(req, res) {
   const authorization = req.headers.authorization;
   const filteredToken = authorization.replace("Bearer", "").trim();
+  console.log(filteredToken)
   try{
     const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
-  console.log(dados)
+    const email = dados.email
     const shoppingCarts = await db.collection("shopping").findOne({email});
     res.send(shoppingCarts.cart);
   } catch(e){
