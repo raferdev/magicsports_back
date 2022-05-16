393e9cec75d4c3895540b0cc2c9eee9fb9aa1fef
Rafael <rafinha_fern@hotmail.com>
2022-05-16 02:04:44 -0300

    mudanças para acessar no heroku

--- controllers/shoppingCartController.js
+++ controllers/shoppingCartController.js
@@ -1,7 +1,12 @@
+import jwt from "jsonwebtoken";
 import db from "../database/db.js";
 
 async function shoppingCartController(req, res) {
-  const email = req.email;
+  const authorization = req.headers.authorization;
+  const token = authorization.replace("Bearer", "").trim();
+  const decoded = jwt.decode(token,{complete: true})
+  console.log(decoded.header);
+  console.log(decoded.payload)
   try{
     const shoppingCarts = await db.collection("shopping").findOne({email});
     res.send(shoppingCarts.cart);
--- routes/shoppingRouter.js
+++ routes/shoppingRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import deleteShoppingCart from "../controllers/deleteShoppingCart.js";
 import shoppingCartController from "../controllers/shoppingCartController.js";
+import tokenValidation from "../middlewares/tokenVerifyMiddleware.js";
 
 const shoppingRouter = express.Router();
 
-shoppingRouter.get("/shopping-cart",shoppingCartController)
-shoppingRouter.delete("/shopping-cart",deleteShoppingCart)
+shoppingRouter.get("/shopping-cart",tokenValidation,shoppingCartController)
+shoppingRouter.delete("/shopping-cart",tokenValidation,deleteShoppingCart)
 export default shoppingRouter;
