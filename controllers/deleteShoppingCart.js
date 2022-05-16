e19c967a78ba0d0308748562c74498f48389352c
Rafael <rafinha_fern@hotmail.com>
2022-05-16 04:31:22 -0300

    funão  do mongodb no deletecontroller

--- controllers/deleteShoppingCart.js
+++ controllers/deleteShoppingCart.js
@@ -5,10 +5,11 @@ async function deleteShoppingCart(req,res) {
     const authorization = req.headers.authorization;
     const filteredToken = authorization.replace("Bearer", "").trim();
     const id = req.query.id;
+    console.log
     try {
         const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
         const email = dados.email;
-       await db.collection("shopping").update({email},{$pull:{id}})
+       await db.collection("shopping").update({email},{"$pull":{id}});
         res.sendStatus(201);
     } catch(e) {
         res.sendStatus(500);
