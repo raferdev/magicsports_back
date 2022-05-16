36bacd25b9b880c2001af2675e31dcc541d9189e
Rafael <rafinha_fern@hotmail.com>
2022-05-16 05:34:13 -0300

    ultima tentativa delete carrinho

--- controllers/deleteShoppingCart.js
+++ controllers/deleteShoppingCart.js
@@ -5,12 +5,15 @@ async function deleteShoppingCart(req, res) {
   const authorization = req.headers.authorization;
   const filteredToken = authorization.replace("Bearer", "").trim();
   const id = req.query.id;
+
   try {
     const dados = jwt.verify(filteredToken, process.env.JWT_SECRET);
     const email = dados.email;
+
     await db
       .collection("shopping")
-      .updateMany({ email }, { $pull: { cart: { id } }}, { multi: true });
+      .updateMany({ email }, { $pull: { "cart": { id } } }, { multi: true });
+
     res.sendStatus(201);
   } catch (e) {
     res.sendStatus(500);
